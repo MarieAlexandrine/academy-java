2bbf21a270cb803798790a9d962c03eea10b6026
MarieAlexandrine <mariealex.iradukunda@gmail.com>
2024-09-25 23:34:34 -0400

    initial commit

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -9,6 +9,7 @@ public class Strings {
         String color1 ="White";
         String color2 ="Yellow";
         String food ="Pizza";
+        String message1 = "This is a test";
 
         //To pass the test case let us assume the food will be "Pizza"
 
@@ -17,7 +18,17 @@ public class Strings {
         System.out.println(pluralnoun1 + " are " + color2);
         System.out.println("I like " + food);
         System.out.println("Do " + pluralnoun2 + " like them too?");
-        
+        System.out.println(color2.length());
+        System.out.println(color2.substring(0,4));
+        System.out.println(color2.indexOf("o"));
+        System.out.println(message1.length());
+        System.out.println(message1.substring (0,3));
+        System.out.println(message1.indexOf ("is"));
+        System.out.println(message1.substring(5));
+        System.out.println(message1.indexOf ("Hello"));
+        String s1 = "Miss you!";
+        int len = s1.length();
+        System.out.println(len);
         // Now come up with your own silly poem!
 	}
 }
