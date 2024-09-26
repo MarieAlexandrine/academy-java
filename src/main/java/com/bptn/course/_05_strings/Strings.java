4a51e378cd5ad7bede51227f14bd9886856431ea
MarieAlexandrine <mariealex.iradukunda@gmail.com>
2024-09-25 21:35:53 -0400

    silly poem

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -3,6 +3,21 @@ package com.bptn.course._05_strings;
 public class Strings {
 
 	public static void main(String[] args) {
-		
+		// fill these in with silly words/strings (don't read the poem yet)
+        String pluralnoun1 = "Eyes";
+        String pluralnoun2 = "Eggs";
+        String color1 ="White";
+        String color2 ="Yellow";
+        String food ="Pizza";
+
+        //To pass the test case let us assume the food will be "Pizza"
+
+        // Run to see the silly poem!
+        System.out.println("Roses are " + color1);
+        System.out.println(pluralnoun1 + " are " + color2);
+        System.out.println("I like " + food);
+        System.out.println("Do " + pluralnoun2 + " like them too?");
+        
+        // Now come up with your own silly poem!
 	}
 }
